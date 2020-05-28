f2d88550f879c0561db20b1312a936f8829101ac
Artem Belov <inctnce@icloud.com>
2020-05-28 16:42:11 +0600

    Initial Commit

---
+++ Integral(Task4)/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Integral(Task4)
+//
+//  Created by Артем Белов on 28.05.2020.
+//  Copyright © 2020 Артем Белов. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

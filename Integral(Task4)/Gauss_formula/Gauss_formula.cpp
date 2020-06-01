f6224bb35464ed8bec505380cc02e84d8e43e155
inctnce <inctnce@icloud.com>
2020-06-01 06:13:32 +0600

    Gauss

--- Integral(Task4)/Gauss_formula/Gauss_formula.cpp
+++ Integral(Task4)/Gauss_formula/Gauss_formula.cpp
@@ -5,10 +5,12 @@
 //  Created by Артем Белов on 01.06.2020.
 //  Copyright © 2020 Артем Белов. All rights reserved.
 //
+#include "../f/f.hpp"
 #include "../LUP/LUP.hpp"
 #include "../Cardano/Cardano_formula.hpp"
 
 #include <cmath>
+#include <iostream>
 
 #include "Gauss_formula.hpp"
 
@@ -18,40 +20,43 @@ double Gauss_formula() {
 
     // Найдено аналитически
     double *Mu = new double[6];
-    Mu[0] = 2.348811462565555; Mu[1] = 2.296615652286315; Mu[2] = 3.248070708233503;
-    Mu[3] = 5.265293569136416; Mu[4] = 9.170386299579257; Mu[5] = 16.69642746957879;
+    Mu[0] = 2.34881146256555;  Mu[1] = 2.53149679854287;  Mu[2]  = 3.730881953316422;
+    Mu[3] = 6.310962062637622; Mu[4] = 11.48080931348324; Mu[5] = 21.84180247930516;
     
     int     *p   = new int[N+1];
     double  *b   = new double[N];
-    double  *abc = new double[N];
-    double  *x   = new double[N];
-    double **A   = new double *[N];
+    double  *a   = new double[N];
+    double  *A   = new double[N];
+    double  *t   = new double[N];
+    double **M   = new double *[N];
     for (int i = 0; i < N; ++i) {
-        A[i] = new double[N];
+        M[i] = new double[N];
     }
-    b[0]    = Mu[3]; b[1]    = Mu[4]; b[2]    = Mu[5];
+    b[0]    = -Mu[3]; b[1]   = -Mu[4]; b[2]   = -Mu[5];
     
-    A[0][0] = Mu[0]; A[0][1] = Mu[1]; A[0][2] = Mu[2];
-    A[1][0] = Mu[1]; A[1][1] = Mu[2]; A[1][2] = Mu[3];
-    A[2][0] = Mu[2]; A[2][1] = Mu[3]; A[2][2] = Mu[4];
+    M[0][0] = Mu[0]; M[0][1] = Mu[1]; M[0][2] = Mu[2];
+    M[1][0] = Mu[1]; M[1][1] = Mu[2]; M[1][2] = Mu[3];
+    M[2][0] = Mu[2]; M[2][1] = Mu[3]; M[2][2] = Mu[4];
     
-    LUPDecompose(A, p, N);
-    LUPSolve(A, p, b, abc, N);
-    Cardano_formula(x, abc[2], abc[1], abc[0]);
+    LUPDecompose(M, p, N);
+    LUPSolve(M, p, b, a, N);
+    
+    std::cout << a[2] << " " << a[1] << " " << a[0] << "\n";
+    
+    Cardano_formula(t, a[2], a[1], a[0]);
+    
+    std::cout << t[0] << " " << t[1] << " " << t[2];
     
     b[0]    = Mu[0];        b[1]    = Mu[1];        b[2]    = Mu[2];
+    
+    
  
-    A[0][0] = 1;            A[0][1] = 1;            A[0][2] = 1;
-    A[1][0] = x[0];         A[1][1] = x[1];         A[1][2] = x[2];
-    A[2][0] = pow(x[0], 2); A[2][1] = pow(x[1], 2); A[2][2] = pow(x[2], 2);
+    M[0][0] = 1;            M[0][1] = 1;            M[0][2] = 1;
+    M[1][0] = t[0];         M[1][1] = t[1];         M[1][2] = t[2];
+    M[2][0] = pow(t[0], 2); M[2][1] = pow(t[1], 2); M[2][2] = pow(t[2], 2);
 
-    LUPDecompose(A, p, N);
-    LUPSolve(A, p, b, x, N);
+    LUPDecompose(M, p, N);
+    LUPSolve(M, p, b, A, N);
     
-//    std::cout << x[0] + x[1] + x[2] << std::endl;
-    
-//    std::cout << std::fixed;
-//    std::cout << std::setprecision(54) << x[0] << " " << std::setprecision(50) << x[1] << " " << std::setprecision(49) << x[2] << std::endl;
-    
-    return x[0] + x[1] + x[2];
+    return A[0]*f(t[0]) + A[1]*f(t[1]) + A[2]*f(t[2]);
 }
--- Integral(Task4)/main.cpp
+++ Integral(Task4)/main.cpp
@@ -6,22 +6,16 @@
 //  Copyright © 2020 Артем Белов. All rights reserved.
 //
 
+/*
+https://www.wolframalpha.com/input/?i=integrate+%28%282.5*cos%282*x%29*exp%282*x%2F3%29%2B4*sin%283.5*x%29*exp%28-3*x%29%2B3*x%29%2F%28x+-+0.1%29%5E%280.2%29%29+dx+from+x%3D0.1+to+2.3
+ */
 // Ответ Wolfram: 3.57886
-// Совпадает с точным решением, если разбить на 6682 узлов, при этом h = = 0.000329292
-
-
-
+// Совпадает с точным решением, если разбить на 6682 узлов, при этом h = 0.000329292
 
 #include <iostream>
-#include <cmath>
-
-#include "LUP/LUP.hpp"
-#include "constants.cpp"
-
 
 #include "Gauss_formula/Gauss_formula.hpp"
 #include "Newton_Cotes/Newton_Cotes.hpp"
-
 
 
 int main() {
